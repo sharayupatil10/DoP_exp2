518ac1223af06e8b2d491caa3211731245d53377
Student <Student@16DITB110B-01.SVV.local>
2024-08-13 12:45:49 +0530

    commit

---
+++ SP1.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Printing numbers from 1 to 10:");
+
+        // Use a for loop to print numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
